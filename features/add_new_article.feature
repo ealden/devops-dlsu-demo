086c2378527f462041b1c748a5328da6be1ca510
Ealden Esto E. Escañan <ealden@escanan.com>
2019-07-13 03:39:31 +0800

    Pass title and text from feature file

--- features/add_new_article.feature
+++ features/add_new_article.feature
@@ -2,8 +2,8 @@ Feature: Add New Article
 
   Scenario: Successfully Add New Article
     Given I want to add a new article
-    And   my article has the title: The Fox and the Dog
-    And   my article has the text: The quick brown fox jumps over the lazy dog
+    And   my article has the title "The Fox and the Dog"
+    And   my article has the text "The quick brown fox jumps over the lazy dog"
     When  I try to add my new article
     Then  I must see my new article added
 
--- features/step_definitions/add_new_article_steps.rb
+++ features/step_definitions/add_new_article_steps.rb
@@ -2,12 +2,12 @@ Given 'I want to add a new article' do
   site.articles.new.load
 end
 
-Given 'my article has the title: The Fox and the Dog' do
-  site.articles.new.title.set 'The Fox and the Dog'
+Given 'my article has the title {string}' do |title|
+  site.articles.new.title.set title
 end
 
-Given 'my article has the text: The quick brown fox jumps over the lazy dog' do
-  site.articles.new.text.set 'The quick brown fox jumps over the lazy dog'
+Given 'my article has the text {string}' do |text|
+  site.articles.new.text.set text
 end
 
 When  'I try to add my new article' do
